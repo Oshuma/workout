ec6478d3c6e3e824a7ca066edd3ea7df5e5c2659
Dale Campbell <oshuma@gmail.com>
2019-07-15 22:44:23 -0400

    Be explicit about ordering of routines in a workout. Fixes #10

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,8 +1,9 @@
 class Workout < ApplicationRecord
   belongs_to :user
-  has_many :routines, dependent: :destroy
+  has_many :routines, -> { order(created_at: :asc).includes(:routine_type) },
+                      dependent: :destroy
 
   validates :date, presence: true
 
-  scope :order_by_entry, -> { order(date: :desc).order(created_at: :asc) }
+  scope :order_by_entry, -> { order(date: :desc).order(created_at: :desc) }
 end
